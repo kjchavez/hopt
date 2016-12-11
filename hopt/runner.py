8138abfa4e4abcde156c4d510271315943cfdf7a
Kevin Chavez <kevin.j.chavez@gmail.com>
2016-12-11 11:40:34 -0800

    create trial directory and metadata file

--- hopt/runner.py
+++ hopt/runner.py
@@ -1,8 +1,13 @@
-from hopt.experiment import Experiment
-from hopt.experiment_pb2 import ExperimentDef
+from __future__ import print_function
 
 import argparse
+import os
+import time
+import uuid
+
 from google.protobuf import text_format
+from hopt.experiment import Experiment
+from hopt.experiment_pb2 import ExperimentDef
 
 
 def parse_args():
@@ -22,11 +27,43 @@ def format_results(params, upper_bound, iteration=None):
     out += "\nUpper bound on value = %s\n" % (str(upper_bound),)
     return out
 
+def create_trial_directory(root, params):
+    """ Creates a new directory for storing info about a trial.
+
+    Arguments
+        root: root output directory, must already exist
+        experiment_name: name of the experiment which this trial belongs to
+        params: dictionary of parameters used for this trial
+
+    Returns:
+        name of new directory
+    """
+    TRIAL_METADATA_FILENAME = 'METADATA'
+    # Directory name is based on either: timestamp, counter, or uuid
+    dirname = 'trial-' + str(uuid.uuid4()).split('-')[-1]
+    full_name = os.path.join(root, dirname)
+    os.mkdir(full_name)
+
+    # Add a metadata file with parameters and creation timestamp
+    with open(os.path.join(full_name, TRIAL_METADATA_FILENAME), 'w') as fp:
+        print("Created at:", time.strftime("%d %b %Y %H:%M:%S"), file=fp)
+        print("\nParameters:", file=fp)
+        print("-"*80, file=fp)
+        for key, value in params.items():
+            print("%s = %s" % (key, str(value)), file=fp)
+        print("-"*80, file=fp)
+
+    return dirname
 
 def run_once(experiment):
     params = experiment.sample_parameters()
+
+    # Create a new directory for data from this trial.
+    root = experiment.output_directory
+    dirname = create_trial_directory(experiment.name, params)
+
     value = experiment.evaluate_fn(params, time_limit=None)
-    print format_results(params, value, iteration=1)
+    print(format_results(params, value, iteration=1))
 
 
 def main():
